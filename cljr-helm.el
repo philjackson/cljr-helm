26f0f098f104c1e3ad345b6995b4ebc61bdd1507
Phil Jackson <phil@shellarchive.co.uk>
2015-03-26 11:08:53 +0000

    Suggest different keybinding.

--- cljr-helm.el
+++ cljr-helm.el
@@ -28,7 +28,7 @@
 ;; Remembering key bindings for cljr is hard, especially the less
 ;; frequently used ones - this should help with that.
 
-;; Simply bind `cljr-helm` to a key (I'd suggest C-c C-r) in Clojure
+;; Simply bind `cljr-helm` to a key (I'd suggest C-c r) in Clojure
 ;; mode, and you're ready to go.
 
 ;;; Code:
