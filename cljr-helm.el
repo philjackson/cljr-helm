916d070503d0f484f36626c7a42f156b737e3fab
Phil Jackson <phil@shellarchive.co.uk>
2015-04-25 21:05:40 +0100

    Rely on the new cljr structure.

--- cljr-helm.el
+++ cljr-helm.el
@@ -4,7 +4,7 @@
 
 ;; Author   : Phil Jackson <phil@shellarchive.co.uk>
 ;; URL      : https://github.com/philjackson/cljr-helm
-;; Version  : 0.6
+;; Version  : 0.7
 ;; Keywords : helm, clojure, refactor
 ;; Package-Requires: ((clj-refactor "0.13.0") (helm "1.5.6"))
 
@@ -33,58 +33,24 @@
 
 ;;; Code:
 
-(defvar cljr-helm-options
-  '(("ad: add declaration" . cljr-add-declaration)
-    ("ai: add import to ns" . cljr-add-import-to-ns)
-    ("ap: add project dependency" . cljr-add-project-dependency)
-    ("am: add missing libspec" . cljr-add-missing-libspec)
-    ("ar: add require to ns" . cljr-add-require-to-ns)
-    ("au: add use to ns" . cljr-add-use-to-ns)
-    ("cn: clean ns" . cljr-clean-ns)
-    ("cc: cycle coll" . cljr-cycle-coll)
-    ("ci: cycle if" . cljr-cycle-if)
-    ("cp: cycle privacy" . cljr-cycle-privacy)
-    ("cs: cycle stringlike" . cljr-cycle-stringlike)
-    ("ct: cycle thread" . cljr-cycle-thread)
-    ("dk: destructure keys" . cljr-destructure-keys)
-    ("el: expand let" . cljr-expand-let)
-    ("ef: extract function" . cljr-extract-function)
-    ("fu: find usages" . cljr-find-usages)
-    ("hd: hotload dependency" . cljr-hotload-dependency)
-    ("il: introduce let" . cljr-introduce-let)
-    ("mf: move form" . cljr-move-form)
-    ("ml: move to let" . cljr-move-to-let)
-    ("pc: project clean" . cljr-project-clean)
-    ("pf: promote function" . cljr-promote-function)
-    ("rf: rename file" . cljr-rename-file)
-    ("rl: remove let" . cljr-remove-let)
-    ("rs: rename symbol" . cljr-rename-symbol)
-    ("rr: remove unused requires" . cljr-remove-unused-requires)
-    ("ru: replace use" . cljr-replace-use)
-    ("sn: sort ns" . cljr-sort-ns)
-    ("sp: sort project dependencies" . cljr-sort-project-dependencies)
-    ("sr: stop referring" . cljr-stop-referring)
-    ("tf: thread first all" . cljr-thread-first-all)
-    ("th: thread" . cljr-thread)
-    ("tl: thread last all" . cljr-thread-last-all)
-    ("ua: unwind all" . cljr-unwind-all)
-    ("uw: unwind" . cljr-unwind)
-    ("rd: remove debug fns" . cljr-remove-debug-fns)))
-
 (defvar helm-source-cljr
-  '((name . "cljr functions")
+  '((name . "cljr functions hlel")
     (init . (lambda ()
-              (helm-init-candidates-in-buffer 'global (mapcar 'car cljr-helm-options))))
+              (helm-init-candidates-in-buffer 'global
+                (mapcar (lambda (c)
+                          (concat (car c) ": " (cadr (cdr c))))
+                        cljr--all-helpers))))
     (candidates-in-buffer)
     (persistent-action . ignore)
-    (action  . (("Run" . (lambda (candidate)
-                           (call-interactively
-                            (cdr (assoc candidate cljr-helm-options)))))))))
+    (action . (("Run" . (lambda (candidate)
+                          (string-match "^\\(.+?\\): " candidate)
+                          (call-interactively
+                           (cadr (assoc (match-string 1 candidate) cljr--all-helpers)))))))))
 
 ;;;###autoload
 (defun cljr-helm ()
   (interactive)
-  (helm-other-buffer 'helm-source-cljr "*cljr*"))
+  (helm-other-buffer 'cljr-helm-source "*cljr*"))
 
 (provide 'cljr-helm)
 
